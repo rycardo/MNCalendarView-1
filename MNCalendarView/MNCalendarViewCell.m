58370e3331a3f389d16c865b89668ce8c5b85395
Rycardo <rycardo.cousteau@yahoo.ca>
2016-09-20 17:59:47 -0400

    updated CalendarViewCell
    added my code for colours and circle

--- MNCalendarView/MNCalendarViewCell.m
+++ MNCalendarView/MNCalendarViewCell.m
@@ -32,7 +32,7 @@ NSString *const MNCalendarViewCellIdentifier = @"MNCalendarViewCellIdentifier";
   if (self = [super initWithFrame:frame]) {
     self.backgroundColor = UIColor.whiteColor;
     self.contentView.backgroundColor = UIColor.clearColor;
-    
+
     self.titleLabel = [[UILabel alloc] initWithFrame:self.bounds];
     self.titleLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
     self.titleLabel.font = [UIFont systemFontOfSize:14.f];
@@ -41,34 +41,69 @@ NSString *const MNCalendarViewCellIdentifier = @"MNCalendarViewCellIdentifier";
     self.titleLabel.textAlignment = NSTextAlignmentCenter;
     self.titleLabel.userInteractionEnabled = NO;
     self.titleLabel.backgroundColor = [UIColor clearColor];
-    
+    self.titleLabel.numberOfLines = 0;
+
     [self.contentView addSubview:self.titleLabel];
-    
-    self.selectedBackgroundView = [[UIView alloc] initWithFrame:self.bounds];
+
+    /*
+     *  MNCalendarView Customise the look for selected days
+     *  Set any layer, colour, etc you want here.
+     */
+    UIView *backgroundView = [[UIView alloc] initWithFrame:self.bounds];
+    backgroundView.layer.backgroundColor = UIColor.clearColor.CGColor;
+    backgroundView.backgroundColor = UIColor.clearColor;
+
+    CALayer *circle         = [CALayer layer];
+    circle.backgroundColor  = appMainActionColour.CGColor;//appAlternateActionColour.CGColor;
+    circle.borderColor      = appMainActionColour.CGColor;//appAlternateActionColour.CGColor;
+    circle.borderWidth      = 2.0;
+    circle.frame            = CGRectInset(self.bounds, 10, 10);
+    circle.cornerRadius     = (circle.frame.size.width / 2);
+    circle.masksToBounds    = YES;
+
+
+    [backgroundView.layer insertSublayer:circle atIndex:0];
+
+
+    self.selectedBackgroundView = backgroundView; //[[UIView alloc] initWithFrame:self.bounds];
     self.selectedBackgroundView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
-    self.selectedBackgroundView.backgroundColor = [UIColor colorWithRed:0.23f green:0.61f blue:1.f alpha:1.f];
+    //self.selectedBackgroundView.backgroundColor = [UIColor colorWithRed:0.23f green:0.61f blue:1.f alpha:1.f];
+
+    //self.selectedBackgroundView.backgroundColor = [UIColor clearColor];
   }
   return self;
 }
 
 - (void)layoutSubviews {
   [super layoutSubviews];
-  
+
   self.contentView.frame = self.bounds;
   self.selectedBackgroundView.frame = self.bounds;
 }
 
 - (void)drawRect:(CGRect)rect {
-  CGContextRef context = UIGraphicsGetCurrentContext();
-  
-  CGColorRef separatorColor = self.separatorColor.CGColor;
-  
-  CGFloat pixel = 1.f / [UIScreen mainScreen].scale;
-  MNContextDrawLine(context,
-                    CGPointMake(0.f, self.bounds.size.height),
-                    CGPointMake(self.bounds.size.width, self.bounds.size.height),
-                    separatorColor,
-                    pixel);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    CGColorRef separatorColor = self.separatorColor.CGColor;
+
+    CGFloat pixel = 1.0 / [UIScreen mainScreen].scale;
+
+    MNContextDrawLine(context,
+                      CGPointMake(0.0, (self.bounds.size.height - 1.0)),
+                      CGPointMake(self.bounds.size.width, (self.bounds.size.height - 1.0)),
+                      separatorColor,
+                      pixel);
+
+//  CGContextRef context = UIGraphicsGetCurrentContext();
+//
+//  CGColorRef separatorColor = self.separatorColor.CGColor;
+//
+//  CGFloat pixel = 1.f / [UIScreen mainScreen].scale;
+//  MNContextDrawLine(context,
+//                    CGPointMake(0.f, self.bounds.size.height),
+//                    CGPointMake(self.bounds.size.width, self.bounds.size.height),
+//                    separatorColor,
+//                    pixel);
 }
 
 @end
